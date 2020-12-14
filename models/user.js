5b0cbfce58d5c6fe45658e9f5f0545b019e34dc4
lucyefranco <lucyefranco@gmail.com>
2020-12-13 16:06:48 -0800

    adds admin to user model. allows registration, but admin column null

--- models/user.js
+++ models/user.js
@@ -53,6 +53,9 @@ module.exports = (sequelize, DataTypes) => {
           msg: 'Password must be between 8 and 99 characters'
         }
       }
+    },
+    admin: {
+      type: DataTypes.BOOLEAN
     }
   }, {
     sequelize,
